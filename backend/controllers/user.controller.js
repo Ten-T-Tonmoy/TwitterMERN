6524e0b1497989ca9f17e4c076329c418a2c21d7
Ten-T-Tonmoy <stu01754@gmail.com>
2025-02-04 02:15:23 +0600

    hectic userUpdate controller done

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -45,7 +45,7 @@ export const followOrUnfollow = async (req, res) => {
       //updating following and followers
       await User.findByIdAndUpdate(req.user._id, { $pull: { following: id } });
 
-      res.status(200).json({ message: "User unfollowed successfully" });
+      return res.status(200).json({ message: "User unfollowed successfully" });
     } else {
       //follow
 
@@ -63,7 +63,7 @@ export const followOrUnfollow = async (req, res) => {
 
       await newNotif.save();
 
-      res.status(200).json({ message: "User followed successfully" });
+      return res.status(200).json({ message: "User followed successfully" });
     }
   } catch (error) {
     console.log("follow/unfollow error controller", error.message);
@@ -83,8 +83,67 @@ export const suggestedUsers = (req, res) => {
   }
 };
 
-export const userUpdate = (req, res) => {
+//----------------------- update User -------------------------------
+
+export const userUpdate = async (req, res) => {
+  const { fullname, email, username, currentPassword, newPassword, bio, link } =
+    req.body;
+
+  let { profileImg, coverImg } = req.body;
+  const userToUpdateId = req.user._id;
   try {
+    let userToUpdate = await User.findById(userToUpdateId);
+
+    if (!userToUpdate) {
+      return res.status(404).json({ message: "user Not Found" });
+    }
+    if (
+      (!newPassword && currentPassword) ||
+      (newPassword && !currentPassword)
+    ) {
+      return res.status(400).json({ message: "Fill both password fields" });
+    }
+    if (currentPassword && newPassword) {
+      const isMatched = await bcrypt.compare(
+        currentPassword,
+        userToUpdate.password
+      );
+      if (!isMatched) {
+        return res.status(400).json({
+          message: "wrong password. Please Provide correct passowrd.",
+        });
+      }
+      if (newPassword.length < 6) {
+        return res
+          .status(400)
+          .json({ message: "password must be 6 char long" });
+      }
+      const salt = await bcrypt.genSalt(10);
+      userToUpdate.password = await bcrypt.hash(newPassword, salt);
+    }
+
+    //cloudinary to upload and manage images!?
+
+    //fix this later
+    if (coverImg) {
+      if (userToUpdate.coverImg) {
+      }
+    }
+
+    userToUpdate.fullname = fullname || userToUpdate.fullname;
+    userToUpdate.username = username || userToUpdate.username;
+    userToUpdate.email = email || userToUpdate.email;
+    userToUpdate.bio = bio || userToUpdate.bio;
+    userToUpdate.link = link || userToUpdate.link;
+    userToUpdate.profileImg = profileImg || userToUpdate.profileImg;
+    userToUpdate.coverImg = coverImg || userToUpdate.coverImg;
+
+    userToUpdate = await userToUpdate.save();
+
+    //null set for safety
+    userToUpdate.password = undefined; //better than null
+
+    return res.status(200).json(userToUpdate);
   } catch (error) {
     console.log("user Updater error controller", error.message);
     res.status(500).json({
